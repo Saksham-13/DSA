e66b49dd788e2866d16d14cc69e5b58cd3654c0d
Saksham-13 <sakshamalok13@gmail.com>
2022-01-10 22:44:19 +0530

    binarysearch

---
+++ binarysearch.py
@@ -0,0 +1,44 @@
+def cieling_of_number(list1,target):
+    start = 0
+    end = len(list1)-1
+    while(start<=end):
+        mid = (start + end)//2
+
+        if(list1[mid] == target):
+             return list1[mid]
+        elif (list1[mid]<target):
+            start = mid +1
+        elif(list1[mid]>target):
+            end = mid-1
+        print(list1[start:end+1])
+    return list1[start]
+def binary_search(list1,target):
+    start = 0
+    end = len(list1)-1
+    while(start<=end):
+        mid = (start + end)//2
+
+        if(list1[mid] == target):
+             return list1[mid]
+        elif (list1[mid]<target):
+            start = mid +1
+        elif(list1[mid]>target):
+            end = mid-1
+        print(list1[start:end+1])
+    return -1
+def floor_of_number(list1,target):
+    start = 0
+    end = len(list1)-1
+    while(start<=end):
+        mid = (start + end)//2
+
+        if(list1[mid] == target):
+             return list1[mid]
+        elif (list1[mid]<target):
+            start = mid +1
+        elif(list1[mid]>target):
+            end = mid-1
+        print(list1[start:end+1])
+    return list1[end]
+
+print(floor_of_number([12,13,17,18,24],15))
