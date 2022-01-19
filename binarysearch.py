69daec1391511ad8df013e5b6f04ea99034c320e
Saksham-13 <sakshamalok13@gmail.com>
2022-01-20 00:24:41 +0530

    added ine comment

--- binarysearch.py
+++ binarysearch.py
@@ -1,3 +1,4 @@
+#binary search questions
 def cieling_of_number(list1,target):
     start = 0
     end = len(list1)-1
@@ -11,7 +12,7 @@ def cieling_of_number(list1,target):
         elif(list1[mid]>target):
             end = mid-1
         print(list1[start:end+1])
-    return list1[start]
+    return list1[start]  
 def binary_search(list1,target):
     start = 0
     end = len(list1)-1
