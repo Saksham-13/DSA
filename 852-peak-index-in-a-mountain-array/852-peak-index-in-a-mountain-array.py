7ca1a298dadf939068edbef281bf0d52c917b81b
Saksham ALok <88273114+Saksham-13@users.noreply.github.com>
2022-01-30 22:37:25 +0530

    Time: 127 ms (5.15%), Space: 14.5 MB (73.00%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,7 +1,22 @@
+from operator import indexOf
+
+
 class Solution(object):
     def peakIndexInMountainArray(self, arr):
         """
         :type arr: List[int]
         :rtype: int
         """
-        return arr.index(max(arr))
+    
+        start = 0
+        end = len(arr)-1
+        while(start<=end):
+            mid = (start + end)//2
+
+            if(arr[mid]>arr[mid-1] and arr[mid]>arr[mid+1]):
+                return mid
+            elif (arr[mid]>arr[mid -1]):
+                start = mid +1
+            else:
+                end = mid
+            
